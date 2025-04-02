29bd2cc420228e74f1a9f9ccac6eac5f8270067d
anacarolcg0 <anacarolcg0@gmail.com>
2025-04-01 22:59:33 -0300

    01-04-2025

---
+++ src/main/java/br/com/fiap/_2tdspwapisecurity/model/UserRole.java
@@ -0,0 +1,18 @@
+package br.com.fiap._2tdspwapisecurity.model;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+
+}
