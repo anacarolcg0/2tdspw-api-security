3de073fc14909dc1a6daee57c2204f3cc64a188b
anacarolcg0 <anacarolcg0@gmail.com>
2025-04-29 22:52:31 -0300

    29-04-2025

--- src/main/java/br/com/fiap/_2tdspwapisecurity/Application.java
+++ src/main/java/br/com/fiap/_2tdspwapisecurity/Application.java
@@ -2,8 +2,10 @@ package br.com.fiap._2tdspwapisecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // habilita o suporte a cache do Spring
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/br/com/fiap/_2tdspwapisecurity/service/UserService.java
@@ -0,0 +1,75 @@
+package br.com.fiap._2tdspwapisecurity.service;
+
+import br.com.fiap._2tdspwapisecurity.model.User;
+import br.com.fiap._2tdspwapisecurity.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //CRUD
+
+    // Create
+    @Transactional
+    @CachePut(value = "users", key = "#result.id")
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Read Id
+    @Cacheable(value = "users", key = "#id")
+    public User readUserById(UUID id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    // Read All
+    @Cacheable(value = "users", key = "'all'")
+    public List<User> readAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Update
+    @Transactional
+    @CachePut(value = "users", key = "#result.id")
+    public User updateUser(UUID id, User user) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            return null;
+        }
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    // Delete
+    @Transactional
+    @CacheEvict(value = "users", key = "#id")
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+        //(um ou outro método)
+        cleanCacheOfAllUsers();
+        //cleanAllUsersFromCache();
+    }
+
+    // Métodos auxiliares do CacheEvict
+    // Atualiza o cache de usuários
+    @CacheEvict(value = "users", key = "'all'")
+    public void cleanCacheOfAllUsers() {}
+
+    // Apaga todos os usuários do cache
+    @CacheEvict(value = "users", allEntries = true)
+    public void cleanAllUsersFromCache() {}
+}
